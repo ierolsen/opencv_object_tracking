9fa91fe26008d016f6b9116f2a8ce31102c0f5b0
Erol <ierolsen@outlook.com>
2020-03-03 18:23:11 +0300

    version2.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This repository contains a part of code that is part of my project and will track red objects with the servo motor.
 
-## Here is some functions I used
+## Here are some functions I used
 
 * **cv2.rectangle()** draws a box 
 	```python
@@ -23,5 +23,37 @@ This repository contains a part of code that is part of my project and will trac
 * **cv2.boundingRect()** returns the 4 points of the bounding box
 
 
-## when the code works:
+## when the version 1.1 works:
 ![middle_point](https://user-images.githubusercontent.com/30235603/74566304-3fa0c680-4f84-11ea-8045-f019609c73b2.png)
+
+## and when the version 2.0 works:
+
+
+
+## what is the difference between version1.0 and version2.0 ?
+
+I just added some morphological transformations to video. They are called erode and dilate. 
+
+-> Erode 
+it rodes away the boundaries of foreground object and used to diminish the features of the image.
+
+-> Dilate
+it increases the object area and used to accentuate features.
+
+and then i defined another morphological functions which is called ``` cv2.MORPH_OPEN```, ```cv2.MORPH_CLOSE```
+
+-> ``` cv2.MORPH_OPEN``` firstly, erosion operator is applied and then dilatation operator is applied. it means they work together.
+
+-> ``` cv2.MORPH_CLOSE``` It is useful in closing small holes inside the foreground objects, or small black points on the object.
+
+
+I think the important difference of the code is here:
+
+morphologyEx() function takes the dilation which is created by ``` cv2.dilate ``` as a source. 
+dilate function makes clean and big area therefore, the colorful object is found clearly.
+and the finally, i gave to ``` cv2.findContours ``` as a source that is called closing and returned by ``` cv2.MORPH_CLOSE ``` function.
+
+## Conclusion
+
+the first version finds the colorful object traditional way. it means, only there were classical functions
+but the version2.0 contains more morphological functions that finding out the area of colorful objects.
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# version 2.0
+
 import cv2
 import numpy as np
 
@@ -13,8 +15,8 @@ while True:
     high_color = np.array([180,255,255])
 
     # blue color
-    # low_color = np.array([110,50,50])
-    # high_color = np.array([130,255,255])
+    #low_color = np.array([110,50,50])
+    #high_color = np.array([130,255,255])
 
     # green color
     # low_color = np.array([25, 52, 72])
@@ -23,32 +25,42 @@ while True:
 
 
     color_mask = cv2.inRange(hsv_frame, low_color, high_color)
+    res = cv2.bitwise_and(frame, frame, mask=color_mask)
 
-    res = cv2.bitwise_and(hsv_frame, hsv_frame, mask=color_mask)
 
+    kernel = np.ones((5,5), np.uint8)
+    erosion = cv2.erode(color_mask, kernel, iterations=1)
+    dilation = cv2.dilate(color_mask, kernel, iterations=1)
 
-    contours, hierarchy  = cv2.findContours(color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+
+    contours, _ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
-    x_medium=0
-    y_medium=0
+    x_medium = 0
+    y_medium = 0
     for cnt in contours:
-
         (x, y, w, h) = cv2.boundingRect(cnt)
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        x_medium = int((x + x + w) / 2) #finding center
-        y_medium = int((y + y+h)/2)
+        x_medium = int((x + x + w) / 2)  # finding center
+        y_medium = int((y + y + h) / 2)
 
         break
 
-
-    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 255), 1) #drawing a line in the middle
+    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 255), 1)  # drawing a line in the middle
     cv2.line(frame, (0, y_medium), (640, y_medium), (0, 255, 255), 1)
+
+
     cv2.imshow("###frame###", frame)
     cv2.imshow('mask',color_mask)
     cv2.imshow('res', res)
+    cv2.imshow('erosion', erosion)
+    cv2.imshow('dilation', dilation)
+    cv2.imshow('opening', opening)
+    cv2.imshow('closing', closing)
 
     key = cv2.waitKey(1)
 
